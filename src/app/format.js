b0605a4a0c063be5bbc9f68e6cb729ecec4aa04c
Sam Gillet-Garas <sgilletgaras@gmail.com>
2021-12-07 04:17:21 +0100

    solution for date format issues

--- src/app/format.js
+++ src/app/format.js
@@ -1,12 +1,30 @@
 export const formatDate = (dateStr) => {
+  
+  // #1 [Bug report] - Bills ------------------------------------------------------------------------------------------------
+  //https://www.w3schools.com/jsref/jsref_parse.asp
+
+  const regEx = /^\d{4}-\d{2}-\d{2}$/; // regEx pour regler prob sur firefox, pas necessaire sur chrome
+
+  if (Date.parse(dateStr) === NaN || dateStr === ""|| !dateStr.match(regEx)) {
+    return "Date Invalide";
+  }
+
   const date = new Date(dateStr)
-  const ye = new Intl.DateTimeFormat('fr', { year: 'numeric' }).format(date)
-  const mo = new Intl.DateTimeFormat('fr', { month: 'short' }).format(date)
-  const da = new Intl.DateTimeFormat('fr', { day: '2-digit' }).format(date)
+  const ye = new Intl.DateTimeFormat('fr', {
+    year: 'numeric'
+  }).format(date)
+  const mo = new Intl.DateTimeFormat('fr', {
+    month: 'short'
+  }).format(date)
+  const da = new Intl.DateTimeFormat('fr', {
+    day: '2-digit'
+  }).format(date)
   const month = mo.charAt(0).toUpperCase() + mo.slice(1)
+
   return `${parseInt(da)} ${month.substr(0,3)}. ${ye.toString().substr(2,4)}`
+
 }
- 
+
 export const formatStatus = (status) => {
   switch (status) {
     case "pending":
