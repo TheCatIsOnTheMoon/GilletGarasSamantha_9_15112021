ef8693b03f30fc0abc85d7344b8b25122f137779
Sam Gillet-Garas <sgilletgaras@gmail.com>
2021-12-13 13:16:22 +0100

    Bill page test redaction

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -5,6 +5,8 @@ import BillsUI from "../views/BillsUI.js"
 import {
   bills
 } from "../fixtures/bills.js"
+import Bill from "../containers/Bills.js"
+import firebase from "../__mocks__/firebase"
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -24,6 +26,107 @@ describe("Given I am connected as an employee", () => {
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
+    })
+  })
+
+  //test click on the new bills button
+  describe("When I click on the button to create a new bill", () => {
+    test("Then a new bill form should be rendered", () => {
+      const html = BillsUI({
+        data: bills
+      })
+      document.body.innerHTML = html
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({
+          pathname
+        })
+      }
+      const firestore = null
+      const bill = new Bill({
+        document,
+        onNavigate,
+        firestore,
+        bills,
+        localStorage: window.localStorage
+      })
+
+      const handleClickNewBillButton = jest.fn(bill.handleClickNewBillButton)
+      const newBillButton = screen.getByTestId('btn-new-bill')
+      newBillButton.addEventListener('click', handleClickNewBillButton)
+      userEvent.click(newBillButton)
+      expect(handleClickNewBillButton).toHaveBeenCalled()
+
+      const newBillForm = screen.getByTestId('form-new-bill')
+      expect(newBillForm).toBeTruthy()
+
+    })
+  })
+
+  //test click on the eye button
+  describe('When I am on Bills Page and I click on the icon eye', () => {
+    test('A modal should open', () => {
+      const html = BillsUI({
+        data: bills
+      })
+      document.body.innerHTML = html
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({
+          pathname
+        })
+      }
+      const firestore = null
+      const bill = new Bill({
+        document,
+        onNavigate,
+        firestore,
+        bills,
+        localStorage: window.localStorage
+      })
+
+      const handleClickIconEye = jest.fn(bill.handleClickIconEye)
+      const eye = screen.getByTestId('icon-eye')
+      eye.addEventListener('click', handleClickIconEye)
+      userEvent.click(eye)
+      expect(handleClickIconEye).toHaveBeenCalled()
+
+      const modale = screen.getByTestId('modaleFile')
+      expect(modale).toBeTruthy()
+    })
+  })
+
+  //test click on disconnect button
+
+  // ajouter un test d'intégration GET Bills.
+  describe("Given I am a user connected as employee", () => {
+    describe("When I navigate to Bills", () => {
+      test("fetches bills from mock API GET", async () => {
+        const getSpy = jest.spyOn(firebase, "get")
+        const bills = await firebase.get()
+        expect(getSpy).toHaveBeenCalledTimes(1)
+        expect(bills.data.length).toBe(4)
+      })
+      test("fetches bills from an API and fails with 404 message error", async () => {
+        firebase.get.mockImplementationOnce(() =>
+          Promise.reject(new Error("Erreur 404"))
+        )
+        const html = BillsUI({
+          error: "Erreur 404"
+        })
+        document.body.innerHTML = html
+        const message = await screen.getByText(/Erreur 404/)
+        expect(message).toBeTruthy()
+      })
+      test("fetches messages from an API and fails with 500 message error", async () => {
+        firebase.get.mockImplementationOnce(() =>
+          Promise.reject(new Error("Erreur 500"))
+        )
+        const html = BillsUI({
+          error: "Erreur 500"
+        })
+        document.body.innerHTML = html
+        const message = await screen.getByText(/Erreur 500/)
+        expect(message).toBeTruthy()
+      })
     })
   })
 })
