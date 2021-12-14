3302809da6f4e1ae75743296a7d370c7b96cab03
Samantha Gillet-Garas <sgilletgaras@gmail.com>
2021-12-14 09:14:53 +0100

    better fix for date chrono display

--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -1,11 +1,11 @@
-import VerticalLayout from './VerticalLayout.js'
-import ErrorPage from "./ErrorPage.js"
-import LoadingPage from "./LoadingPage.js"
+import VerticalLayout from "./VerticalLayout.js";
+import ErrorPage from "./ErrorPage.js";
+import LoadingPage from "./LoadingPage.js";
 
-import Actions from './Actions.js'
+import Actions from "./Actions.js";
 
 const row = (bill) => {
-  return (`
+  return `
     <tr>
       <td>${bill.type}</td>
       <td>${bill.name}</td>
@@ -16,21 +16,26 @@ const row = (bill) => {
         ${Actions(bill.fileUrl)}
       </td>
     </tr>
-    `)
-}
+    `;
+};
 
 const rows = (data) => {
   return data && data.length ? data.map((bill) => row(bill)).join("") : "";
 };
 
+// #1 [Bug report] -------------------------------------------------------------------------------------------------
+//https://stackoverflow.com/questions/10123953/how-to-sort-an-object-array-by-date-property
+// const rows = (data) =>
+//   data && data.length
+//     ? data
+//         .sort((a, b) => new Date(b.date) - new Date(a.date))
+//         .map((bill) => row(bill))
+//         .join("")
+//     : "";
+//------------------------------------------------------------------------------------------------
 
-export default ({
-  data: bills,
-  loading,
-  error
-}) => {
-
-  const modal = () => (`
+export default ({ data: bills, loading, error }) => {
+  const modal = () => `
     <div class="modal fade" id="modaleFile" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
         <div class="modal-content">
@@ -45,26 +50,26 @@ export default ({
         </div>
       </div>
     </div>
-  `)
+  `;
 
   if (loading) {
-    return LoadingPage()
+    return LoadingPage();
   } else if (error) {
-    return ErrorPage(error)
+    return ErrorPage(error);
   }
 
   // #1 [Bug report] -------------------------------------------------------------------------------------------------
   //https://stackoverflow.com/questions/10123953/how-to-sort-an-object-array-by-date-property
 
-  if (bills) {
-    bills.sort(function (a, b) {
-      return new Date(b.date) - new Date(a.date);
-    });
-  }
+  // if (bills) {
+  //   bills.sort(function (a, b) {
+  //     return new Date(b.date) - new Date(a.date);
+  //   });
+  // }
 
   // ----------------------------------------------------------------------------------------------------------------------
 
-  return (`
+  return `
     <div class='layout'>
       ${VerticalLayout(120)}
       <div class='content'>
@@ -91,5 +96,5 @@ export default ({
         </div>
       </div>
       ${modal()}
-    </div>`)
-}
+    </div>`;
+};
