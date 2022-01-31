54eafc98adb78f4449dc8b6b17f37f4acebb92bd
Samantha Gillet-Garas <sgilletgaras@gmail.com>
2022-01-31 22:21:21 +0100

    prettier

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -1,88 +1,88 @@
-import {
-  ROUTES_PATH
-} from '../constants/routes.js'
-import Logout from "./Logout.js"
+import { ROUTES_PATH } from "../constants/routes.js";
+import Logout from "./Logout.js";
 
 export default class NewBill {
-  constructor({
-    document,
-    onNavigate,
-    firestore,
-    localStorage
-  }) {
-    this.document = document
-    this.onNavigate = onNavigate
-    this.firestore = firestore
-    const formNewBill = this.document.querySelector(`form[data-testid="form-new-bill"]`)
-    formNewBill.addEventListener("submit", this.handleSubmit)
-    const file = this.document.querySelector(`input[data-testid="file"]`)
-    file.addEventListener("change", this.handleChangeFile)
-    this.fileUrl = null
-    this.fileName = null
+  constructor({ document, onNavigate, firestore, localStorage }) {
+    this.document = document;
+    this.onNavigate = onNavigate;
+    this.firestore = firestore;
+    const formNewBill = this.document.querySelector(
+      `form[data-testid="form-new-bill"]`
+    );
+    formNewBill.addEventListener("submit", this.handleSubmit);
+    const file = this.document.querySelector(`input[data-testid="file"]`);
+    file.addEventListener("change", this.handleChangeFile);
+    this.fileUrl = null;
+    this.fileName = null;
     new Logout({
       document,
       localStorage,
-      onNavigate
-    })
+      onNavigate,
+    });
   }
-  handleChangeFile = e => {
-
-    const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
-    const filePath = e.target.value.split(/\\/g)
-    const fileName = filePath[filePath.length - 1]
-
+  handleChangeFile = (e) => {
+    const file = this.document.querySelector(`input[data-testid="file"]`)
+      .files[0];
+    const filePath = e.target.value.split(/\\/g);
+    const fileName = filePath[filePath.length - 1];
 
     // #3 [Bug Hunt] - Bills]---------------------------------------------------------------------------------------------------------
 
-    const fileExtention = fileName.split('.').pop();
+    const fileExtention = fileName.split(".").pop();
 
-    console.log(fileExtention)
+    console.log(fileExtention);
 
     if (["jpg", "jpeg", "png"].includes(fileExtention)) {
-
-      this.firestore
-        .storage
+      this.firestore.storage
         .ref(`justificatifs/${fileName}`)
         .put(file)
-        .then(snapshot => snapshot.ref.getDownloadURL())
-        .then(url => {
-          this.fileUrl = url
-          this.fileName = fileName
-        })
-      return true
+        .then((snapshot) => snapshot.ref.getDownloadURL())
+        .then((url) => {
+          this.fileUrl = url;
+          this.fileName = fileName;
+        });
+      return true;
     }
 
-    alert(" Wrong file type. Accepted files extentions are .jpg, .jpeg or .png only ");
+    alert(
+      " Wrong file type. Accepted files extentions are .jpg, .jpeg or .png only "
+    );
 
     this.fileUrl = null;
     this.fileName = null;
-    e.target.value = '';
+    e.target.value = "";
 
     // --------------------------------------------------------------------------------------------------------------
-  }
+  };
 
-
-
-  handleSubmit = e => {
-    e.preventDefault()
-    console.log('e.target.querySelector(`input[data-testid="datepicker"]`).value', e.target.querySelector(`input[data-testid="datepicker"]`).value)
-    const email = JSON.parse(localStorage.getItem("user")).email
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(
+      'e.target.querySelector(`input[data-testid="datepicker"]`).value',
+      e.target.querySelector(`input[data-testid="datepicker"]`).value
+    );
+    const email = JSON.parse(localStorage.getItem("user")).email;
     const bill = {
       email,
       type: e.target.querySelector(`select[data-testid="expense-type"]`).value,
       name: e.target.querySelector(`input[data-testid="expense-name"]`).value,
-      amount: parseInt(e.target.querySelector(`input[data-testid="amount"]`).value),
+      amount: parseInt(
+        e.target.querySelector(`input[data-testid="amount"]`).value
+      ),
       date: e.target.querySelector(`input[data-testid="datepicker"]`).value,
       vat: e.target.querySelector(`input[data-testid="vat"]`).value,
-      pct: parseInt(e.target.querySelector(`input[data-testid="pct"]`).value) || 20,
-      commentary: e.target.querySelector(`textarea[data-testid="commentary"]`).value,
+      pct:
+        parseInt(e.target.querySelector(`input[data-testid="pct"]`).value) ||
+        20,
+      commentary: e.target.querySelector(`textarea[data-testid="commentary"]`)
+        .value,
       fileUrl: this.fileUrl,
       fileName: this.fileName,
-      status: 'pending'
-    }
-    this.createBill(bill)
-    this.onNavigate(ROUTES_PATH['Bills'])
-  }
+      status: "pending",
+    };
+    this.createBill(bill);
+    this.onNavigate(ROUTES_PATH["Bills"]);
+  };
 
   // not need to cover this function by tests
   /* istanbul ignore next */
@@ -92,9 +92,9 @@ export default class NewBill {
         .bills()
         .add(bill)
         .then(() => {
-          this.onNavigate(ROUTES_PATH['Bills'])
+          this.onNavigate(ROUTES_PATH["Bills"]);
         })
-        .catch(error => error)
+        .catch((error) => error);
     }
-  }
+  };
 }
